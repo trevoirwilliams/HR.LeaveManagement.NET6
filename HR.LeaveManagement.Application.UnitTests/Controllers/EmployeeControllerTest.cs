7f074fa6a71accda5184d2aeed666d6508367916
ivan.stefanov <ivanstefanov.it@gmail.com>
2022-06-30 15:16:12 +0300

    Add unit test

---
+++ HR.LeaveManagement.Application.UnitTests/Controllers/EmployeeControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.API.Controllers;
+using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.DTOs.Employee;
+using HR.LeaveManagement.Identity.Models;
+using HR.LeaveManagement.Identity.Services;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.Controllers
+{
+    public class EmployeeControllerTest
+    {
+        [Fact]
+        public void GetAllEmployees()
+        {
+            
+            //arrange
+            var employeeController = new EmployeeController(Mock.Of<IUserService>());
+            //act
+            var result = employeeController.Get();
+            //assert
+            Assert.NotNull(result);
+        }
+    }
+}
